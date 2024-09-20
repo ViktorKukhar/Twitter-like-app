f81c0f00bc2dd1bc0c9455339f1afca3c9c209ed
Viktor Kukhar <kukharviktor@icloud.com>
2024-09-20 14:31:28 +0300

    add :validatable to devise, style error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable
+  devise :database_authenticatable, :registerable, :validatable
 
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
