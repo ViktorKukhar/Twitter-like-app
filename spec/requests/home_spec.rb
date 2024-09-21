ddca2197cf90907358a47233bc2153236cefd507
Viktor Kukhar <109076253+ViktorKukhar@users.noreply.github.com>
2024-09-21 22:56:14 +0300

    Update home_spec.rb

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Public", type: :request do
+RSpec.describe "Home", type: :request do
   describe "GET /index" do
     it "returns http success" do
       get home_index_path
