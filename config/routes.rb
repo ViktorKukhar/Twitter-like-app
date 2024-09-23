555ef77abf3a70ef28412687c79f8ed698d32e38
Viktor Kukhar <kukharviktor@icloud.com>
2024-09-23 11:16:19 +0300

    delete session controller, upd test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
   }
   root "home#index"
-  resources :home
+  get 'home', to: 'home#index'
   get "up" => "rails/health#show", as: :rails_health_check
 end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Public", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get home_index_path
+      get home_path
 
       expect(response).to have_http_status(:success)
     end
