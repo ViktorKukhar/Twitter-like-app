09c418519ef321bc85a660c5cb277badc61a0c65
Viktor Kukhar <kukharviktor@icloud.com>
2024-09-27 00:30:52 +0300

    add documentation to flash_controller.js

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -6,34 +6,38 @@ export default class extends Controller {
     timeout: { type: Number, default: 5000 }
   }
 
+  // determines what will be executed from start
   connect() {
     this.fadeIn();
     this.startTimeout();
   }
 
+  // appearance of message
   fadeIn() {
     this.element.classList.add("opacity-0", "transition-opacity", "duration-500");
 
     setTimeout(() => {
       this.element.classList.remove("opacity-0");
       this.element.classList.add("opacity-100");
-    }, 50); // Small delay to trigger the transition
+    }, 50);
   }
 
+  // time to fadeOut.
+  // set in shared/_flash_messages.html.erb by data-flash-timeout-value="5000"
   startTimeout() {
     setTimeout(() => {
       this.fadeOut();
     }, this.timeoutValue);
   }
 
+  // disappearance of message
   fadeOut() {
-    // Apply the fade-out transition
-    this.element.classList.remove("opacity-100"); // Start fading out
-    this.element.classList.add("opacity-0"); // Transition to invisible
+    this.element.classList.remove("opacity-100");
+    this.element.classList.add("opacity-0");
 
-    // After the transition, remove the element from the DOM
+
     setTimeout(() => {
       this.element.remove();
-    }, 500); // Match the transition duration with the time (500ms)
+    }, 500);
   }
 }
