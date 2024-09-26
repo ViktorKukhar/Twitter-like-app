334648ade697426522c738f3921117bcc1516055
Viktor Kukhar <kukharviktor@icloud.com>
2024-09-26 19:11:52 +0300

    change root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
-  root "home#index"
   get "up" => "rails/health#show", as: :rails_health_check
 end
