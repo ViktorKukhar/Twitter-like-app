c1002a4c5bd9380b386e5a32fdf7173b79e36167
Viktor Kukhar <kukharviktor@icloud.com>
2024-10-06 01:07:51 +0300

    feat(tweet_input): add char count

--- app/javascript/controllers/tweet_controller.js
+++ app/javascript/controllers/tweet_controller.js
@@ -2,11 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tweet"
 export default class extends Controller {
-  static targets = ["textarea"]
+  static targets = ["textarea", "submit", "count"]
 
   connect() {
-    this.textareaTarget.style.overflow = 'hidden';
+    this.maxLength = 50; // app/models/tweet.rb/ validates :body, length: { minimum: 1, maximum: 50 }
     this.resize();
+    this.checkLength();
   }
 
   resize() {
@@ -16,5 +17,33 @@ export default class extends Controller {
 
   clearTextarea() {
     this.textareaTarget.value = "";
+    this.checkLength();
+  }
+
+  checkLength() {
+    const textLength = this.textareaTarget.value.length;
+    const remainingChars = this.maxLength - textLength;
+
+    this.countTarget.innerHTML = `${remainingChars}`;
+
+    // Check if the text length exceeds the limit
+    if (remainingChars < 0) {
+      this.textareaTarget.classList.add("!text-red-500");
+      this.countTarget.classList.add("text-red-500")
+      this.submitTarget.disabled = true;
+    } else if (textLength === 0) {
+      this.submitTarget.disabled = true;
+    } else {
+      this.textareaTarget.classList.remove("!text-red-500");
+      this.countTarget.classList.remove("text-red-500")
+      this.submitTarget.disabled = false;
+    }
+
+    if (this.submitTarget.disabled) {
+      this.submitTarget.classList.add("opacity-50");
+    } else {
+      this.submitTarget.classList.remove("opacity-50");
+    }
+
   }
 }
