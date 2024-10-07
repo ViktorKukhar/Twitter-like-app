02d8dffa32395cb6ce7dfa179218118faeeef419
Viktor Kukhar <kukharviktor@icloud.com>
2024-10-07 23:26:26 +0300

    refactor(stimulus): rename sidebar controller to dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="sidebar"
+// Connects to data-controller="dropdown"
 export default class extends Controller {
   static targets = ["dropdown"]
 
