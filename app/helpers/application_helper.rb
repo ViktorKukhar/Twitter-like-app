60fe05274431349b44685774a1e0389bb4e045f6
Viktor Kukhar <kukharviktor@icloud.com>
2024-10-07 03:56:37 +0300

    feat(header): create helper for changing header page name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,19 @@ module ApplicationHelper
       'tweet-error'
     end
   end
+
+  def page(current_page)
+    case current_page
+    when root_path
+      content_tag(:p, 'For you')
+    when new_user_session_path
+      content_tag(:p, 'Log in')
+    when new_user_registration_path
+      content_tag(:p, 'Sign up')
+    when tweet_path
+      content_tag(:p, 'Tweet')
+    else
+      content_tag(:p, 'Twitter')
+    end
+  end
 end
